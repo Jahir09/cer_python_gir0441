d0150e32627e1803c6b8724df1bc4236af36a06b
Jahir09 <106194501+Jahir09@users.noreply.github.com>
2022-09-20 08:49:07 -0500

    Delete lab2_1_1_6.py

--- lab2_1_1_6.py
+++
@@ -1,8 +0,0 @@
-'''
-Descripcion: Programa muy facil de la funcion Print
-Autor: Jahir Artemio Casas Espínola 
-Fecha: 20 de Septiembre del 2022
-'''
-print('Hola Mundo!!!')
-print('Soy Jahir')
-print('Bienvenido a Python')
