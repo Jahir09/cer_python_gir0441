49cb1d522d20ac8d91a62854039ce13fa704e41d
Jahir09 <106194501+Jahir09@users.noreply.github.com>
2022-09-20 08:49:54 -0500

    Add files via upload

---
+++ modulo02/lab2_1_1_6.py
@@ -0,0 +1,8 @@
+'''
+Descripcion: Programa muy facil de la funcion Print
+Autor: Jahir Artemio Casas Espínola 
+Fecha: 20 de Septiembre del 2022
+'''
+print('Hola Mundo!!!')
+print('Soy Jahir')
+print('Bienvenido a Python')
