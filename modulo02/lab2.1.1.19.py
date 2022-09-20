092ca8ce3e61897b7ac22b8fd267f34bec8a7f97
Jahir09 <106194501+Jahir09@users.noreply.github.com>
2022-09-20 09:44:55 -0500

    Add files via upload

---
+++ modulo02/lab2.1.1.19.py
@@ -0,0 +1,17 @@
+'''
+Descripcion:
+Autor: Jahir Artemio Casas Espínola 
+Fecha: 20 de Septiembre del 2022
+'''
+'''
+print("    *" *2)
+print("   * *" *2)
+print("  *   *" *2)
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+'''
+
+print("    *", "\n   * *", "\n  *   *", "\n *     *","\n***   ***", "\n  *   *", "\n  *   *", "\n  *****")
