9ef2a0407aa9da8bbaf2af1eb762bf4832df1c48
Jahir09 <106194501+Jahir09@users.noreply.github.com>
2022-09-22 10:35:59 -0500

    Add files via upload

---
+++ modulo02/lab2.1.1.18.py
@@ -0,0 +1,8 @@
+'''
+Descripcion: Funcion end y sep
+Autor: Jahir Artemio Casas Espínola
+Fecha: 22 de Septiembre del 2022
+'''
+
+print("Fundamentos","Programacion", "en", sep=("***"),end="...")
+print("Pyton")
